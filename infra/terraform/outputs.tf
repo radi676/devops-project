64b2dc51ac816e90673d50ad0e9812b4cbc01a63
Radina Ovcharova <93722607+radi676@users.noreply.github.com>
2025-12-27 19:18:29 +0200

    Add output var on stage

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -11,7 +11,7 @@ output "cluster_name" {
 }
 
 output "cluster_location" {
-  value = google_container_cluster.primary.location
+  value = google_container_cluster.primary.zone
 }
 
 output "cluster_endpoint" {
