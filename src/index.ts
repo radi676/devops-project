40f48ece0c6e98b601b7e1b4b78c4a9625fb38ce
Radina Ovcharova <93722607+radi676@users.noreply.github.com>
2025-12-28 12:56:43 +0200

    Update code

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,8 @@ const server = http.createServer((req, res) => {
 							: 'development',
 			time: new Date().toISOString(),
 			utc_time: new Date().toUTCString(),
+			my_property: 'My value',
+			
 		})
 	)
 })
